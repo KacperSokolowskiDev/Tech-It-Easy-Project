d39b500e7259d73e4783a21480837356486493c6
Kacper Sokolowski <Kacperbxl@gmail.com>
2020-10-06 13:59:15 +0200

    Added the form js script and a new body disposition

---
+++ index.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("form");
+
+form.addEventListener('submit', (event) => { 
+    alert("Thank you, we will contact you soon when the webiste is ready");
+    event.preventDefault();
+    form.reset();
+})
